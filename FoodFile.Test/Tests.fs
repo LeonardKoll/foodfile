d487aa01ab1753a8ea0d9b2c76e6434aa2ed8b1b
Leonard Koll <leko@netlight.com>
2020-06-14 21:40:06 +0200

    Example insertion test based

--- FoodFile.Test/Tests.fs
+++ FoodFile.Test/Tests.fs
@@ -1,8 +1,28 @@
-module Tests
+namespace FoodFile.Test
 
 open System
-open Xunit
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Microsoft.Extensions.Configuration
+open FoodFile
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+[<TestClass>]
+type TestClass () =
+
+    [<ClassInitialize>]
+    static member public PrepareElastic (context:TestContext) =
+        
+        let config = (new ConfigurationBuilder()).AddJsonFile("appSettings.test.json").Build()
+
+        let host = config.GetValue<string>("elastic")
+        let entityindex = config.GetValue<string>("entityindex")
+        let memberindex = config.GetValue<string>("memberindex")
+        let creationcmd = config.GetValue<string>("indexcreationcmd")
+        ElasticService.InitIndices host entityindex memberindex creationcmd
+        
+        new ElasticService(config) :> IElasticService
+        |> fun service -> List.iter (fun entity ->
+                service.WriteEntity entity) FoodFile.TestData.all
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
